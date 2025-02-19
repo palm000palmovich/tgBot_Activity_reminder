69526f673299e32f05828104d55f014538cc1f90
palm000palmovich <ivankiselev6534@gmail.com>
2025-02-19 23:01:56 +0300

    added reaction for command "/start"

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -7,12 +7,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.services.CommandsService;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
+    @Autowired
+    private CommandsService comSer;
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
@@ -28,9 +31,12 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
+            System.out.println(update.message().chat().id());
 
-            if (update.message() != null &&
-            update.message().text() != null){String command = update.message().text();}
+            if (update.message() != null && update.message().text().equals("/start")){
+                 long chatId = update.message().chat().id();
+                 comSer.answerToStart(this.telegramBot, chatId);
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
